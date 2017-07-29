bba87f97c51323045276f9952b8f0f36790b107d
Branislav Lazic <branislav.lazic@codecentric.de>
2017-07-29 19:32:05 +0200

    Test when provided list is empty

--- src/list_elements_with_comma_delimiter.erl
+++ src/list_elements_with_comma_delimiter.erl
@@ -4,5 +4,7 @@
 delimited_elements_as_string(List) when is_list(List) ->
   ValueBeforeTrimming = lists:foldl(fun(Value, Acc) -> Acc ++ Value ++ "," end, "", List),
   lists:sublist(ValueBeforeTrimming, 1, length(ValueBeforeTrimming) - 1);
+delimited_elements_as_string([]) ->
+  "";
 delimited_elements_as_string(_)->
   error.
--- test/elements_with_comma_delimiter_tests.erl
+++ test/elements_with_comma_delimiter_tests.erl
@@ -4,3 +4,6 @@
 
 delimited_elements_as_string_test() ->
   ?assertEqual("1,2,3,4", list_elements_with_comma_delimiter:delimited_elements_as_string(["1", "2", "3", "4"])).
+
+delimited_elements_as_string_empty_list_test() ->
+  ?assertEqual("", list_elements_with_comma_delimiter:delimited_elements_as_string([])).
