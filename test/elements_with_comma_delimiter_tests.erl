b137249a970a5c6a1b78c6daeb52179703b7080f
Branislav Lazic <branislav.lazic@codecentric.de>
2017-07-29 17:33:29 +0200

    Rename to elements_with_comma_delimiter_tests

--- test/elements_with_comma_delimiter_tests.erl
+++ test/elements_with_comma_delimiter_tests.erl
@@ -2,5 +2,5 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-get_delimited_elements_as_string_test() ->
+delimited_elements_as_string_test() ->
   ?assertEqual("1,2,3,4", list_elements_with_comma_delimiter:delimited_elements_as_string(["1", "2", "3", "4"])).
